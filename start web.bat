a2e11972f80bdaf41224a4f7af1ac5f5fe87d123
Timor <Ibrahim.timor.ext@dematic.com>
2016-11-16 10:11:18 +0200

    add new script for start web

---
+++ start web.bat
@@ -0,0 +1,11 @@
+
+ start "" https://jira.dematic.com
+ start "" https://wiki.dematic.com
+ start "" https://hipchat.dematic.com
+ start "" http://gitlab.ops.cld
+ start "" http://jenkins.ops.cld 
+ start "" http://artifactory.ops.cld
+
+ start "" https://wiki.dematic.com/display/DEVOPS/Infrastructure+Configurations
+ start "" http://docs.ansible.com/ 
+ 
